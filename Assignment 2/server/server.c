0c99ecacc4cda1c783d671ff9686d630631ec597
Barun Parua <76466796+Baron105@users.noreply.github.com>
2024-01-18 13:29:26 +0000

    concurrent server

--- Assignment 2/server/server.c
+++ Assignment 2/server/server.c
@@ -3,145 +3,155 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
-#include <sys/socket.h> 
+#include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <fcntl.h>
 
-int main()
-{
-    int sockfd,newsockfd;
+int main() {
+    int sockfd, newsockfd;
     struct sockaddr_in cli_addr, serv_addr;
 
-    // cannot read more than 64 bytes at a time
+    // buffer size < 100 recommended in assignment
     char buffer[64] = {'\0'};
 
     // creating socket
-    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
-        perror("Socket creation failed\n");
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Socket creation failed");
         exit(0);
     }
 
-    // setting params
+    // setting params of server
     serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = INADDR_ANY;
-	serv_addr.sin_port = htons(6969);
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(6969);
 
     // binding
-    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-		perror("Binding failed\n");
-		exit(0);
-	}
+    if (bind(sockfd, (struct sockaddr * ) & serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Binding failed");
+        exit(0);
+    }
 
     // listening limit
     listen(sockfd, 10);
 
-
     // start accepting
-    while(1)
-    {
+    while (1) {
+        // accept connection from client
         int cli_len = sizeof(cli_addr);
-        newsockfd = accept(sockfd, (struct sockaddr*) &cli_addr, &(cli_len));
+        newsockfd = accept(sockfd, (struct sockaddr * ) & cli_addr, & (cli_len));
 
-        if (newsockfd < 0)
-        {
-            perror("Accepting failed\n");
+        if (newsockfd < 0) {
+            perror("Accepting failed");
             exit(0);
         }
 
-        // now start communicating
+        // handling multiple clients using fork
+        if (fork() == 0) {
 
-        // receive the value of key
-        int k;
-        // it is a string, so we need to convert it to int
-        recv(newsockfd, buffer, sizeof(buffer), 0);
-        k = atoi(buffer);
+            // close the original socket    
+            close(sockfd);
 
-        printf("Key received: %d\n", k);
+            // now start communicating
 
-        // get the filename from client
-        char filename[64];
-        recv(newsockfd, filename, sizeof(buffer), 0);
-        memset(buffer, '\0', sizeof(buffer));
+            // receive the value of key
+            int k;
 
-        printf("Filename received: %s\n", filename);
+            // it is received as a string, so we need to convert it to int
+            recv(newsockfd, buffer, sizeof(buffer), 0);
+            k = atoi(buffer);
+            printf("Key received: %d\n", k);
+            memset(buffer, '\0', sizeof(buffer));
 
-        // now create a new file with name <IP.port of client>.txt
-        char tempFilename[256];
-        sprintf(tempFilename, "%s.%d.txt", inet_ntoa(cli_addr.sin_addr), ntohs(cli_addr.sin_port));
-        int tempFile = open(tempFilename, O_CREAT | O_WRONLY | O_TRUNC, 0666);
+            // get the filename from client
+            char filename[64];
+            recv(newsockfd, filename, sizeof(buffer), 0);
+            memset(buffer, '\0', sizeof(buffer));
 
-        printf("Temporary file created: %s\n", tempFilename);
+            printf("Filename received: %s\n", filename);
 
-        // receive and write file data to the temporary file
-        int br = 1;
-        while (1)
-        {
-            br = recv(newsockfd, buffer, sizeof(buffer), 0);
-            printf("br: %d\n", br);
-            if (br < 64 && br > 0)
-            {
-                write(tempFile, buffer, br);
-                break;
-            }
-            else if (br == 0 || br == -1) break;
-            write(tempFile, buffer, br);
-        }
+            // now create a new file with name <IP.port of client>.txt
+            char tempfilename[100];
+            sprintf(tempfilename, "%s.%d.txt", inet_ntoa(cli_addr.sin_addr), ntohs(cli_addr.sin_port));
+            int tempfile = open(tempfilename, O_CREAT | O_WRONLY | O_TRUNC, 0666);
 
-        printf("File data received\n");
+            printf("Temporary file created: %s\n", tempfilename);
 
-        close(tempFile);
+            // receive and write file data to the temporary file
+            int br = 1;
+            while (1) {
+                br = recv(newsockfd, buffer, sizeof(buffer), 0);
+                // printf("br: %d\n", br);
 
-        // encrypt the file
-        // encrypted file will be named <IP.port of client>.txt.enc
-        char encFilename[256];
-        sprintf(encFilename, "%s.%d.txt.enc", inet_ntoa(cli_addr.sin_addr), ntohs(cli_addr.sin_port));
-        int encFile = open(encFilename, O_CREAT | O_WRONLY | O_TRUNC, 0666);
-
-        printf("Encrypted file created: %s\n", encFilename);
-
-        // write encrypted data to the new file
-        int fd = open(tempFilename, O_RDONLY);
-        while ((br = read(fd, buffer, sizeof(buffer))) > 0)
-        {
-            for (int i = 0; i < br; i++)
-            {
-                // caesar cipher
-                // lowercase
-                if (buffer[i] >= 'a' && buffer[i] <= 'z')
-                {
-                    buffer[i] = (buffer[i] - 'a' + k) % 26 + 'a';
+                // if buffer is less than 64, it means that the file has no more data after this
+                // so write the remaining data and break
+                if (br < 64 && br > 0) {
+                    write(tempfile, buffer, br);
+                    break;
                 }
 
-                // uppercase
-                else if (buffer[i] >= 'A' && buffer[i] <= 'Z')
-                {
-                    buffer[i] = (buffer[i] - 'A' + k) % 26 + 'A';
-                }
+                // if br == 0, it means that the file has been completely received
+                // if br == -1, it means that there was an error
+                else if (br == 0 || br == -1) break;
 
-                // else do nothing
+                write(tempfile, buffer, br);
             }
-            write(encFile, buffer, br);
+
+            printf("File data received\n");
+
+            close(tempfile);
+
+            // encrypt the file
+            // encrypted file will be named <IP.port of client>.txt.enc
+            char encfilename[100];
+            sprintf(encfilename, "%s.%d.txt.enc", inet_ntoa(cli_addr.sin_addr), ntohs(cli_addr.sin_port));
+            int encfile = open(encfilename, O_CREAT | O_WRONLY | O_TRUNC, 0666);
+
+            printf("Encrypted file created: %s\n", encfilename);
+
+            // write encrypted data to the new file
+            int fd = open(tempfilename, O_RDONLY);
+            while ((br = read(fd, buffer, sizeof(buffer))) > 0) {
+                for (int i = 0; i < br; i++) {
+                    // caesar cipher
+                    // lowercase
+                    if (buffer[i] >= 'a' && buffer[i] <= 'z') {
+                        buffer[i] = (buffer[i] - 'a' + k) % 26 + 'a';
+                    }
+
+                    // uppercase
+                    else if (buffer[i] >= 'A' && buffer[i] <= 'Z') {
+                        buffer[i] = (buffer[i] - 'A' + k) % 26 + 'A';
+                    }
+
+                    // else do nothing
+                }
+                write(encfile, buffer, br);
+            }
+
+            close(fd);
+
+            printf("File encryption complete\n");
+
+            // now send the encrypted file back to the client
+            int efd = open(encfilename, O_RDONLY);
+            while ((br = read(efd, buffer, sizeof(buffer))) > 0) {
+                send(newsockfd, buffer, br, 0);
+                // printf("br: %d\n", br);
+            }
+
+            memset(buffer, '\0', sizeof(buffer));
+
+            close(efd);
+
+            printf("Encrypted file sent\n\n");
+
+            // close the connection
+            close(newsockfd);
+
+            exit(0);
         }
 
-        close(fd);
-
-        printf("File encrypted\n");
-
-        // now send the encrypted file back to the client
-        int encFd = open(encFilename, O_RDONLY);
-        while ((br = read(encFd, buffer, sizeof(buffer))) > 0)
-        {
-            send(newsockfd, buffer, br, 0);
-            // printf("br: %d\n", br);
-        }
-
-        close(encFd);
-
-        printf("Encrypted file sent\n");
-
-        // close the connection
         close(newsockfd);
     }
 }
