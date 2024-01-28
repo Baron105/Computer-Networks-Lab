ad69cd8585884f838b9dbada3dd3fecdd5631a32
Barun Parua <76466796+Baron105@users.noreply.github.com>
2024-01-28 14:30:10 +0000

    format

--- Assignment 3/client/mailclient.c
+++ Assignment 3/client/mailclient.c
@@ -3,20 +3,23 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
-#include <sys/socket.h> 
+#include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <fcntl.h>
 #include <signal.h>
 
-void set_nonblocking(int sockfd) {
+void set_nonblocking(int sockfd)
+{
     int flags = fcntl(sockfd, F_GETFL, 0);
-    if (flags == -1) {
+    if (flags == -1)
+    {
         perror("fcntl");
         exit(EXIT_FAILURE);
     }
 
-    if (fcntl(sockfd, F_SETFL, flags | O_NONBLOCK) == -1) {
+    if (fcntl(sockfd, F_SETFL, flags | O_NONBLOCK) == -1)
+    {
         perror("fcntl");
         exit(EXIT_FAILURE);
     }
@@ -26,9 +29,9 @@ char buf[2048];
 
 void message_220(int client_socket)
 {
-    int n = recv(client_socket , buf , sizeof(buf) , 0);
+    int n = recv(client_socket, buf, sizeof(buf), 0);
     buf[n] = '\0';
-    printf("%s\n" , buf);
+    printf("%s\n", buf);
 }
 
 int main()
@@ -38,69 +41,64 @@ int main()
     char pop3_port[100];
 
     printf("Enter the server ip : ");
-    scanf("%s" , server_ip);
+    scanf("%s", server_ip);
 
     printf("Enter the smtp port : ");
-    scanf("%s" , smtp_port);
+    scanf("%s", smtp_port);
 
     printf("Enter the pop3 port : ");
-    scanf("%s" , pop3_port);
+    scanf("%s", pop3_port);
 
     char username[100];
     char password[100];
 
     printf("Enter the username : ");
-    scanf("%s" , username);
+    scanf("%s", username);
 
     printf("Enter the password : ");
-    scanf("%s" , password);
+    scanf("%s", password);
 
     int client_socket;
-    struct sockaddr_in server_addr,client_addr;
+    struct sockaddr_in server_addr, client_addr;
 
-    //opening a socket
-    if((client_socket = socket(AF_INET,SOCK_STREAM,0)) < 0)
+    // opening a socket
+    if ((client_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         perror("Unable to create socket\n");
         exit(0);
     }
 
-    //binding the client (optional in this case)
+    // binding the client (optional in this case)
 
     // server info
-    server_addr.sin_family = AF_INET ;
-    inet_aton(server_ip,&server_addr.sin_addr);
+    server_addr.sin_family = AF_INET;
+    inet_aton(server_ip, &server_addr.sin_addr);
     server_addr.sin_port = htons(atoi(smtp_port));
 
-
-
-    while(1)
+    while (1)
     {
         printf("1.Manage mails\n2.Send mail\n3.Quit\n");
 
         int choice;
-        scanf("%d" , &choice);
+        scanf("%d", &choice);
 
-        if(choice == 3)
+        if (choice == 3)
         {
             // send receiver that quit is requested
 
-
-            // also print the message response from server 
+            // also print the message response from server
 
             // break any connection is open
             return 0;
-
         }
 
-        else if(choice == 1)
+        else if (choice == 1)
         {
-
         }
-        else if(choice == 2)
+        else if (choice == 2)
         {
             // connect to the server
-            if((connect(client_socket,(struct sockaddr *)&server_addr,sizeof(server_addr)))<0)
+            if ((connect(client_socket, (struct sockaddr *)&server_addr, sizeof(server_addr))) < 0)
             {
                 perror("Unable to connect to server");
                 exit(0);
@@ -109,11 +107,11 @@ int main()
             // set the socket to non blocking
             set_nonblocking(client_socket);
 
-            // so tcp connection is accepted , so receiver will send the message of 220 
+            // so tcp connection is accepted , so receiver will send the message of 220
             message_220(client_socket);
 
             // if the message is not 220 , then close the connection and exit
-            if(strncmp("220" , buf , 3) != 0)
+            if (strncmp("220", buf, 3) != 0)
             {
                 printf("Error in connection\n");
                 close(client_socket);
@@ -121,13 +119,8 @@ int main()
             }
             // send the HELO message with server domain name as <ip>@<port>
             char message[2048];
-            sprintf(message , "HELO %s@%s\n" , inet_ntoa(server_addr.sin_addr) , smtp_port);
-            send(client_socket , message , strlen(message) , 0);
-
-
-
-
-
+            sprintf(message, "HELO %s@%s\n", inet_ntoa(server_addr.sin_addr), smtp_port);
+            send(client_socket, message, strlen(message), 0);
         }
     }
 }
--- Assignment 3/server/smtpmail.c
+++ Assignment 3/server/smtpmail.c
@@ -8,14 +8,17 @@
 #include <arpa/inet.h>
 #include <fcntl.h>
 
-void set_nonblocking(int *sockfd) {
+void set_nonblocking(int *sockfd)
+{
     int flags = fcntl(*sockfd, F_GETFL, 0);
-    if (flags == -1) {
+    if (flags == -1)
+    {
         perror("fcntl");
         exit(EXIT_FAILURE);
     }
 
-    if (fcntl(*sockfd, F_SETFL, flags | O_NONBLOCK) == -1) {
+    if (fcntl(*sockfd, F_SETFL, flags | O_NONBLOCK) == -1)
+    {
         perror("fcntl");
         exit(EXIT_FAILURE);
     }
@@ -23,32 +26,32 @@ void set_nonblocking(int *sockfd) {
 
 int main()
 {
-    int server_socket , client_socket ;
+    int server_socket, client_socket;
     int new_sock;
-    struct sockaddr_in server_addr  , client_addr ;
+    struct sockaddr_in server_addr, client_addr;
     socklen_t sin_len = sizeof(client_addr);
 
     char buf[2048];
 
     // creating the socket
-    server_socket = socket(AF_INET , SOCK_STREAM , 0);
-    if(server_socket < 0)
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket < 0)
     {
         perror("Error in creating the socket\n");
         exit(1);
     }
 
-    //setting the server address
-    int port ;
+    // setting the server address
+    int port;
     printf("Enter the port number : ");
-    scanf("%d" , &port);
+    scanf("%d", &port);
 
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(port);
 
     // bind the socket
-    if(bind(server_socket , (struct sockaddr *) &server_addr , sizeof(server_addr)) == -1)
+    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
     {
         perror("Error in binding\n");
         close(server_socket);
@@ -56,7 +59,7 @@ int main()
     }
 
     // listen for the connections
-    if(listen(server_socket , 5) == -1)
+    if (listen(server_socket, 5) == -1)
     {
         perror("Error in listening\n");
         close(server_socket);
@@ -65,24 +68,22 @@ int main()
 
     printf("SMTP server ready and listening for connections\n");
 
-    while(1)
+    while (1)
     {
-        // accept the connection 
-        new_sock = accept(server_socket , (struct sockaddr *) &client_addr , &sin_len);
+        // accept the connection
+        new_sock = accept(server_socket, (struct sockaddr *)&client_addr, &sin_len);
 
         printf("check\n");
-        if(new_sock<0)
+        if (new_sock < 0)
         {
             perror("Error in accepting the connection\n");
             close(server_socket);
             exit(EXIT_FAILURE);
         }
 
-
-
         // making a fork for concurrency
-        int pid ;
-        if((pid=fork())==0)
+        int pid;
+        if ((pid = fork()) == 0)
         {
             // close the old socket in the child process
             close(server_socket);
@@ -90,15 +91,10 @@ int main()
             // connection established
             char message[2048] = "220 <Domain_name> Service ready\n";
 
-            send(new_sock , message , strlen(message) , 0);
+            send(new_sock, message, strlen(message), 0);
             close(new_sock);
             exit(0);
-
         }
         close(new_sock);
-
-
     }
-
-
 }
